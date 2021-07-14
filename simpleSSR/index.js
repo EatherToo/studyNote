4aeb511a271706261070940576ad0a08052f2c0c
zhouyz <zhouyz@160.com>
2021-07-15 00:09:42 +0800

    feat: vue开发服务器

--- simpleSSR/index.js
+++ simpleSSR/index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 const Koa = require("koa");
-const compilerSfc = require("@vue/compiler-sfc"); // .vue
-const compilerDom = require("@vue/compiler-dom"); // 模板
+const compilerSfc = require("@vue/compiler-sfc"); // 单文件组件编译
+const compilerDom = require("@vue/compiler-dom"); // 模版编译
 const app = new Koa();
 function rewriteImport(content) {
   return content.replace(/ from ['|"]([^'"]+)['|"]/g, function (s0, s1) {
-    // . ../ /开头的，都是相对路径
+    console.log("s", s0, s1);
     if (s1[0] !== "." && s1[1] !== "/") {
       return ` from '/@modules/${s1}'`;
     } else {
@@ -23,28 +23,23 @@ app.use(async (ctx) => {
   // 首页
   if (url == "/") {
     ctx.type = "text/html";
-    // 直接读取文件然后输出到客户端
     let content = fs.readFileSync("./index.html", "utf-8");
+    content = content.replace(
+      "<script ",
+      `
+      <script>
+        window.process = {env:{ NODE_ENV:'dev'}}
+      </script>
+      <script 
+    `
+    );
     ctx.body = content;
-  } else if (url.endsWith(".js")) { // 读取js文件
+  } else if (url.endsWith(".js")) {
     // js文件
     const p = path.resolve(__dirname, url.slice(1));
     ctx.type = "application/javascript";
     const content = fs.readFileSync(p, "utf-8");
     ctx.body = rewriteImport(content);
-  } else if (url.endsWith(".css")) { // 读取css文件
-    const p = path.resolve(__dirname, url.slice(1));
-    const file = fs.readFileSync(p, "utf-8");
-    const content = `
-    const css = "${file.replace(/\n/g, "")}"
-    let link = document.createElement('style')
-    link.setAttribute('type', 'text/css')
-    document.head.appendChild(link)
-    link.innerHTML = css
-    export default css
-    `;
-    ctx.type = "application/javascript";
-    ctx.body = content;
   } else if (url.startsWith("/@modules/")) {
     // 这是一个node_module里的东西
     const prefix = path.resolve(
@@ -57,11 +52,24 @@ app.use(async (ctx) => {
     const ret = fs.readFileSync(p, "utf-8");
     ctx.type = "application/javascript";
     ctx.body = rewriteImport(ret);
+  } else if (url.endsWith(".css")) {
+    const p = path.resolve(__dirname, url.slice(1));
+    const file = fs.readFileSync(p, "utf-8");
+    const content = `
+    const css = "${file.replace(/\n/g, "")}"
+    let link = document.createElement('style')
+    link.setAttribute('type', 'text/css')
+    document.head.appendChild(link)
+    link.innerHTML = css
+    export default css
+    `;
+    ctx.type = "application/javascript";
+    ctx.body = content;
   } else if (url.indexOf(".vue") > -1) {
     // vue单文件组件
     const p = path.resolve(__dirname, url.split("?")[0].slice(1));
     const { descriptor } = compilerSfc.parse(fs.readFileSync(p, "utf-8"));
-    console.log(p, query)
+
     if (!query.type) {
       ctx.type = "application/javascript";
       // 借用vue自导的compile框架 解析单文件组件，其实相当于vue-loader做的事情
@@ -86,6 +94,6 @@ app.use(async (ctx) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("listen on 3000");
+app.listen(3001, () => {
+  console.log("听我口令，3001端口，起~~");
 });
---
+++ simpleSSR/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello world</h1>
+  <h2>
+    <span>{{sum}}</span>
+    <button @click="num1++">num1++</button>
+  </h2>
+</template>
+
+<script>
+import {ref,computed} from 'vue'
+export default {
+  setup(){
+    const num1 = ref(1)
+    const num2 = ref(2)
+    const sum = computed(()=> num1.value + num2.value)
+    console.log(sum, '1111111')
+    return {num1,num2,sum}
+  }
+}
+</script>
